edbdff5d6a7342543f7dab24faedf594f1f1016d
Ilya Krasilnikov <kravus27@gmail.com>
2020-01-07 23:27:20 +0300

    all changes

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,16 +1,16 @@
 import stringToNumber from '../app';
 
-test ('if amount is number', () => {
-    let amount = '6';
-    const received = stringToNumber(amount);
-    const expected = 6;
-    expect(received).toBe(expected);
+test('if amount is number', () => {
+  const amount = '6';
+  const received = stringToNumber(amount);
+  const expected = 6;
+  expect(received).toBe(expected);
 });
 
-test ('if amount is not number', () => {
-    let amount = 'frfrf';
-    ;
-    expect(()=>{
-        stringToNumber(amount);
-    }).toThrow(new Error ('tgyhi'));
+test('if amount is not number', () => {
+  const amount = 'frfrf';
+
+  expect(() => {
+    stringToNumber(amount);
+  }).toThrow(new Error('tgyhi'));
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
-let amount = '6';
-export default function stringToNumber (amount) {
-  let amountToNumber = Number(amount);
-  if (isNaN(amountToNumber)) throw new Error ('tgyhi');
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+const amount = '6';
+export default function stringToNumber(amount) {
+  const amountToNumber = Number(amount);
+  if (isNaN(amountToNumber)) throw new Error('tgyhi');
   return amountToNumber;
-};
+}
