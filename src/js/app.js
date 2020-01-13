2d4e273b7f5e50c652092396e5ecb4f09f2f2598
Iliakra <51176758+Iliakra@users.noreply.github.com>
2020-01-13 15:19:06 +0300

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,6 @@
 const amount = '6';
 export default function stringToNumber(amount) {
   const amountToNumber = Number(amount);
-  if (isNaN(amountToNumber)) throw new Error('tgyhi');
+  if (isNaN(amountToNumber)) throw new Error('Некорректные данные: введите число в десятичной системе исчисления!');
   return amountToNumber;
 }
